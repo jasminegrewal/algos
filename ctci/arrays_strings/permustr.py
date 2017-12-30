6e4965697cd48944a7a5a19d465040bc2a9f8c94
Jasmine <jasminegrewal@users.noreply.github.com>
2017-12-30 08:07:39 -0800

    Update permustr.py
    adding comments

--- ctci/arrays_strings/permustr.py
+++ ctci/arrays_strings/permustr.py
@@ -5,21 +5,10 @@ Created on Fri Mar 24 15:30:31 2017
 @author: jasmine
 if one str is permutation of other
 """
-'''method 1
-def ispermut(s1, s2):
-    if len(s1)!=len(s2):
-        return False
-    if ((''.join(sorted(s1))) == (''.join(sorted(s2)))):
-        return True
-    else:
-        return False
-        
-print (ispermut('abcc','abc'))
-
-'''
 def ispermut(s,t):
     if len(s)!=len(t):
         return False
+    # create an int array of len 128(ASCII) with default  0
     letters = [0 for i in range(0,128)]
     for char in s:
         letters[ord(char)]=letters[ord(char)]+1
@@ -27,6 +16,8 @@ def ispermut(s,t):
     for char in t:
         j= ord(char)
         print(letters[j]-1)
+        # if a different char comes values is -1 or if a character count differs value gets negative
+        # also we are checking length initially
         if ((letters[j]-1)<0):
             return False
     return True
